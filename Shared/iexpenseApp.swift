30fb9fb233968d57a865d43fed085c3fea6daa9c
danizavtz <daniellucena@yahoo.com.br>
2023-05-04 20:04:52 -0300

    Initial Commit

---
+++ Shared/iexpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iexpenseApp.swift
+//  Shared
+//
+//  Created by Daniel Lucena Pires on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct iexpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
